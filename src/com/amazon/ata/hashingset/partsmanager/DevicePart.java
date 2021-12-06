2fa23332640d57e109165d13e9cc74392b86edfe
Charles Jordan <charlesjordan@Charless-iMac.local>
2021-12-06 12:51:04 -0600

    guided project complete

--- src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
+++ src/com/amazon/ata/hashingset/partsmanager/DevicePart.java
@@ -1,6 +1,7 @@
 package com.amazon.ata.hashingset.partsmanager;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DevicePart {
     private String manufacturer;
@@ -35,6 +36,20 @@ public class DevicePart {
 
     public void removeDeviceUsedIn(AmazonDevice amazonDevice) {
         devicesUsedIn.remove(amazonDevice);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DevicePart that = (DevicePart) o;
+        return getManufacturer().equals(that.getManufacturer()) &&
+                getManufacturersPartNumber().equals(that.getManufacturersPartNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getManufacturer(), getManufacturersPartNumber());
     }
 
     @Override
